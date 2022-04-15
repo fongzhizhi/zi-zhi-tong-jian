425ff4ab3f0f5f3f39fc079573d755b1b9f03943
unknown <jlc_0086@SZ-JLC.COM>
2022-04-15 19:44:12 +0800

    更新笔记

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import "./styles/main.less";
+import { printStyleLog } from "./utils/util";
+import axios from "axios";
+import { marked } from "marked";
+
+window.onload = () => {
+  loadReadme();
+  doSomething();
+};
+
+function loadReadme() {
+  axios
+    .get("http://localhost:3030/readme")
+    .then((res) => {
+      if (res && res.data) {
+        const readMeHtml = marked(res.data);
+        document.getElementById("readme").innerHTML = readMeHtml;
+      }
+    })
+    .catch((err) => {
+      printStyleLog("Server Error", err);
+    });
+}
+
+function doSomething() {
+  // print something
+  printStyleLog(
+    "Jinx",
+    {
+      name: "Jinx",
+      age: 21,
+    },
+    {
+      color: "#41b883",
+    }
+  );
+}
